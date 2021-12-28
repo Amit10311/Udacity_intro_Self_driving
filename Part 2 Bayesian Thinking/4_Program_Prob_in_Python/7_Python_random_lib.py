6a6642d14b132dd0f8bacc8bb5eb57140a3e0731
Amit Kumar <amitkumar10311@gmail.com>
2021-12-28 14:23:30 +0100

    Update 7_Python_random_lib.py

--- Part 2 Bayesian Thinking/4_Program_Prob_in_Python/7_Python_random_lib.py
+++ Part 2 Bayesian Thinking/4_Program_Prob_in_Python/7_Python_random_lib.py
@@ -1,9 +1,6 @@
 # `random` is a Python "module".
 # It provides functionality associated with randomness / probability. 
-# For now you don't need to know exactly
 
 # what is happening when we write `import random as rd`,
-
-# Focus  on how we USE this module.
 
 import random as r
