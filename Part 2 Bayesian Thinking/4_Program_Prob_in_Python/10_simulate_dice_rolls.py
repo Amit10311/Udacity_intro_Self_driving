eb422a5f471c6ba7a003127ed097b28a2bbb63e8
Amit Kumar <amitkumar10311@gmail.com>
2021-12-28 14:13:43 +0100

    Create 10_simulate_dice_rolls.py

---
+++ Part 2 Bayesian Thinking/4_Program_Prob_in_Python/10_simulate_dice_rolls.py
@@ -0,0 +1,58 @@
+import random as rd
+
+
+def simulate_dice_rolls(N):
+    """
+    Simulates dice rolls
+    
+    Args:
+        N (int): The number of trials
+        
+    Returns:
+        list: roll counts [1,6]
+    """
+    
+    # Create a list to track the 6 options for the roll
+    roll_counts = [0,0,0,0,0,0]
+    
+    for i in range(N):
+        # Randomly select a value from the list (1 to 6)
+        roll = rd.choice([1,2,3,4,5,6]) 
+
+        # Recall indices start at 0 so we need to decrement
+        index = roll - 1
+        
+        roll_counts[index] = roll_counts[index] + 1
+    return roll_counts
+
+def show_roll_data(roll_counts):
+    """
+    Shows the dice roll data
+    
+    Args:
+        roll_counts (list): The roll counts stored in the list
+        
+    Returns:
+        list: roll counts [1,6]
+    """ 
+    # Gets the number of sides of the dice and prints
+    # the side of the die.
+     
+    # enumerate creates the position of the die and the list value
+    
+    for dice_side, frequency in enumerate(roll_counts):
+        print(dice_side + 1, "was rolled", frequency, "times")
+        
+roll_data = simulate_dice_rolls(10000)
+show_roll_data(roll_data)
+
+
+"""  OUTPUT 
+
+1 was rolled 1678 times
+2 was rolled 1638 times
+3 was rolled 1745 times
+4 was rolled 1653 times
+5 was rolled 1653 times
+6 was rolled 1633 times
+"""
