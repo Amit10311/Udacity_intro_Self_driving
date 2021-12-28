37d5769c7cbc11f3d730410c10ca333505bff0f0
Amit Kumar <amitkumar10311@gmail.com>
2021-12-28 14:27:58 +0100

    Update 7_Python_random_lib.py

--- Part 2 Bayesian Thinking/4_Program_Prob_in_Python/7_Python_random_lib.py
+++ Part 2 Bayesian Thinking/4_Program_Prob_in_Python/7_Python_random_lib.py
@@ -19,6 +19,7 @@ print("c is", c)
 
 print("\n Now keep pressing Ctrl+Enter to run this cell many times! \n")
 
+# 7.1 Python number method choice() returns a random item from a list, tuple, or string.
 
 print (" choice([1, 2, 3, 5, 9]) : ", rd.choice([1, 2, 3, 5, 9]))
 print (" \n choice('A String') : ", rd.choice('A String'))
