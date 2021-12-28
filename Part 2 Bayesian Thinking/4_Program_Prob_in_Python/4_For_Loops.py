a3385d2f75f90f6edb4b6c1ad93cd35eefce38e9
Amit Kumar <amitkumar10311@gmail.com>
2021-12-28 13:30:07 +0100

    Update 4_For_Loops.py

--- Part 2 Bayesian Thinking/4_Program_Prob_in_Python/4_For_Loops.py
+++ Part 2 Bayesian Thinking/4_Program_Prob_in_Python/4_For_Loops.py
@@ -4,11 +4,12 @@ print("2 - demonstrating for loop with range()")
 for i in range(6):
     print(i)
     
-
-# 1 - demonstrating for loop
-# 0
-# 1
-# 2
-# 3
-# 4
-# 5 
+"""
+1 - demonstrating for loop
+0
+1
+2
+3
+4
+5
+"""
