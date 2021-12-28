c99bf7799f3b6cd5b5e5eacfdd7c9b5ea924c954
Amit Kumar <amitkumar10311@gmail.com>
2021-12-28 14:03:54 +0100

    Update 5_1_ Enumerating_list.py

--- Part 2 Bayesian Thinking/4_Program_Prob_in_Python/5_1_ Enumerating_list.py
+++ Part 2 Bayesian Thinking/4_Program_Prob_in_Python/5_1_ Enumerating_list.py
@@ -1,3 +1,6 @@
+
+# 1 Method 
+
 print("Enumerating a list... \n")
 
 my_list = [1, 2, 3, "a", "b", "c"]
@@ -5,9 +8,21 @@ my_list = [1, 2, 3, "a", "b", "c"]
 for i, item in enumerate(my_list):
     print("item number", i, "is", item)
     
-    
+# 2 Method     
 print("Another way to enumerate using a list 'method'... \n ")
 
 for item in my_list:
     index = my_list.index(item)  
     print("item", item, "has index", index)
+
+    
+ ## OUTPUTS  
+    
+ """ Enumerating a list... 
+
+item number 0 is 1
+item number 1 is 2
+item number 2 is 3
+item number 3 is a
+item number 4 is b
+item number 5 is c """
