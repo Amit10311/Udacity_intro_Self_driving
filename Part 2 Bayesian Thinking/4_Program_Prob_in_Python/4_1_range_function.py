8542a69205427edb38cc1309a82e27dcc63c45d4
Amit Kumar <amitkumar10311@gmail.com>
2021-12-28 13:33:16 +0100

    Create 4_1_range_function.py

---
+++ Part 2 Bayesian Thinking/4_Program_Prob_in_Python/4_1_range_function.py
@@ -0,0 +1,25 @@
+# This is an example of a nested loop
+# Nested loops are loops inside of loops
+# the left_num will increment slower than the right_num
+
+# How many times will right_num perform full loops? 25 times 
+
+for left_num in range(3):
+    for right_num in range(4):
+        product = left_num * right_num
+        print(left_num, "x", right_num, "=", product)
+        
+"""
+0 x 0 = 0
+0 x 1 = 0
+0 x 2 = 0
+0 x 3 = 0
+1 x 0 = 0
+1 x 1 = 1
+1 x 2 = 2
+1 x 3 = 3
+2 x 0 = 0
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+"""
