f0fe3dfb0bcb291c9a534988f8dd1d31664a02b8
Amit Kumar <amitkumar10311@gmail.com>
2021-12-28 13:42:43 +0100

    Update 5_Lists_and_Loops.py

--- Part 2 Bayesian Thinking/4_Program_Prob_in_Python/5_Lists_and_Loops.py
+++ Part 2 Bayesian Thinking/4_Program_Prob_in_Python/5_Lists_and_Loops.py
@@ -2,6 +2,37 @@ my_list = [1, 2, 3, "a", "b", "c"]
 
 print("my_list is:", my_list)
 
+
+# 2 Prints each element of a list individually   
+
 print("Looping through a list...")
 for item in my_list:
     print("item is", item)
+    
+# 3 Prints the number of elements in a list   
+    
+print("The len function is important!")
+
+num_elements = len(my_list)
+print("my_list has", num_elements, "elements \n")   
+        
+    
+
+    
+
+#### OUTPUT 
+
+# my_list is: [1, 2, 3, 'a', 'b', 'c']
+
+""" 2 OUTPUT 
+Looping through a list...
+item is 1
+item is 2
+item is 3
+item is a
+item is b
+item is """
+
+""" 3 OUTPUT 
+The len function is important!
+my_list has 6 elements """
