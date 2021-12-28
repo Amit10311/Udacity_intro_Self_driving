512fff8d69ccd37faae32c40fd29f0d8038f33dd
Amit Kumar <amitkumar10311@gmail.com>
2021-12-28 13:20:49 +0100

    Create  1_Data_strings.py

---
+++ Part 2 Bayesian Thinking/4_Program_Prob_in_Python/1_Data_strings.py
@@ -0,0 +1,34 @@
+# 1. STRINGS
+
+# In this section you define several variables. These are all defined
+# to be strings. Note that there are several ways to define a string.
+
+print("STRINGS")      # Prints the word STRINGS as the output
+
+# 1.1 Declaring strings with double quotes
+my_string_1 = "hello"
+
+# 1.2 Declaring strings with single quotes
+my_string_2 = 'world'
+
+# Use either three double quotes or three single quotes
+# note in this case you can have single quotes of either type
+# inside the string. This is a powerful feature!
+my_multiline_string = """
+
+Dear World,
+
+Hello. I am a multiline python string.
+I'm enclosed in triple quotes. I'd write
+them here, but that would end the string!
+
+I know! I'll use a slash as an escape character.
+
+Triple quotes look like this: \"\"\" 
+
+Sincerely,
+Python
+"""
+
+newline_character = "\n"
+print("Now run the cell below to see these strings printed!")
