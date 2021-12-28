bf35ae3f7170afb0f92a74b811cbb0f4f2d06104
Amit Kumar <amitkumar10311@gmail.com>
2021-12-28 14:24:11 +0100

    Update 7_Python_random_lib.py

--- Part 2 Bayesian Thinking/4_Program_Prob_in_Python/7_Python_random_lib.py
+++ Part 2 Bayesian Thinking/4_Program_Prob_in_Python/7_Python_random_lib.py
@@ -3,4 +3,18 @@
 
 # what is happening when we write `import random as rd`,
 
-import random as r
+import random as rd 
+
+# 1. "Call the FUNCTION" named "random" and
+# 2. assign the result of each "call to a variable"
+
+a = rd.random()
+b = rd.random()
+c = rd.random()
+print("a is", a)
+print("b is", b)
+print("c is", c)
+
+# random isn't the only function in the random module.
+
+print("\n Now keep pressing Ctrl+Enter to run this cell many times! \n")
