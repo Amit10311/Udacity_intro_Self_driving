f9d1d91705c254d8e46c1f313ee53f6310007ebb
Amit Kumar <amitkumar10311@gmail.com>
2022-01-01 23:57:38 +0100

    Create 1_math.py

---
+++ Part 2 Bayesian Thinking/8_Program_Probab_Distributions/1_math.py
@@ -0,0 +1,12 @@
+import math
+
+radius        = 10.0
+diameter      = 2 * radius
+circumference = 2 * math.pi * radius
+area          = math.pi * radius ** 2
+
+print("Radius is", radius)
+print("Diameter is", diameter)
+print("Circumference is", circumference)
+print("Area is", area)
+
