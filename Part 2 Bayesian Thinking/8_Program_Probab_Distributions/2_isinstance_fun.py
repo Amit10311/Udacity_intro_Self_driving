7745821cc0d07cd55553241c35992d9433175c35
Amit Kumar <amitkumar10311@gmail.com>
2022-01-01 23:58:48 +0100

    Create 2_isinstance_fun.py

---
+++ Part 2 Bayesian Thinking/8_Program_Probab_Distributions/2_isinstance_fun.py
@@ -0,0 +1,13 @@
+sqr_root_2 = math.sqrt(2)
+
+is_sqr_root_2_an_integer = isinstance(sqr_root_2, int)
+print("Is square root two an integer?", is_sqr_root_2_an_integer)
+
+is_sqr_root_2_a_float = isinstance(sqr_root_2, float)
+print("Is square root two a float?", is_sqr_root_2_a_float)
+
+
+"" OUPUT ""
+
+# Is square root two an integer? False
+# Is square root two a float? True
