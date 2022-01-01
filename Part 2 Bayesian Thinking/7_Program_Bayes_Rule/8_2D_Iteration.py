c4be6703285dba3c40a4b3e88993a0d33110fcbb
Amit Kumar <amitkumar10311@gmail.com>
2022-01-01 23:41:25 +0100

    Create 8_2D_Iteration.py

---
+++ Part 2 Bayesian Thinking/7_Program_Bayes_Rule/8_2D_Iteration.py
@@ -0,0 +1,15 @@
+1. Create the world
+import numpy as np
+
+# A 6x5 robot world
+world = np.array([ [0, 0, 0, 1, 0],
+                   [0, 0, 0, 1, 0],
+                   [0, 1, 1, 0, 0],
+                   [0, 0, 0, 0, 1],
+                   [1, 0, 0, 1, 0],
+                   [1, 0, 0, 0, 0] ])
+
+# Print out some information about the world
+print(world)
+
+print('\nThe shape of this array is: ' + str(world.shape))//
